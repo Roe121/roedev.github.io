6ba1962de66456fdf86933c6b46143abffbffc56
Roe Lez <99667815+Roe121@users.noreply.github.com>
2022-11-13 21:10:41 +0100

    Responsivnesse update

---
+++ grocweb/script.js
@@ -0,0 +1,44 @@
+let countDate = new Date('november 15, 2022 00:00:00').getTime();
+function CountDown(){
+
+    let now = new Date().getTime();
+    gap = countDate - now;
+
+    let second = 1000;
+    let minute = second * 60;
+    let hour = minute * 60;
+    let day = hour * 24;
+
+    let d = Math.floor(gap / (day));
+    let h = Math.floor((gap % (day)) / (hour));
+    let m = Math.floor((gap % (hour)) / (minute));
+    let s = Math.floor((gap % (minute)) / (second));
+
+    document.getElementById('day').innerText = d;
+    document.getElementById('hour').innerText = h;
+    document.getElementById('minute').innerText = m;
+    document.getElementById('second').innerText = s;
+
+}
+
+setInterval(function(){
+    CountDown();
+},1000)
+
+
+
+var menu = document.getElementById("bar");
+var item = document.getElementById("item");
+
+item.style.left ='-300px';
+
+menu.onclick = function () {
+
+if (item.style.left =='-300px') {
+    item.style.left ='0'
+}
+else {
+    item.style.left ='-300px'
+}
+
+}
